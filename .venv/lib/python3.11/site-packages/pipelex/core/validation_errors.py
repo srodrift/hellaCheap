2aa7813394046db43dcafb405e8466f5997349b8
sunnyrodriguess <srodrigues_gps@nec.edu>
2025-10-29 20:11:11 -0700

    Clean: removed venv and added to gitignore

--- .venv/lib/python3.11/site-packages/pipelex/core/validation_errors.py
+++
@@ -1,7 +0,0 @@
-from typing_extensions import Protocol
-
-from pipelex.builder.validation_error_data import ConceptDefinitionErrorData
-
-
-class ValidationErrorDetailsProtocol(Protocol):
-    def get_concept_definition_errors(self) -> list[ConceptDefinitionErrorData]: ...
